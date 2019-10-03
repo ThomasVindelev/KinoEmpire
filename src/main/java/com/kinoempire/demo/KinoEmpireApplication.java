e3e463c6a64cb9f4bceee2111e7fa653c02f19b1
ThomasVindelev <thom891b@stud.kea.dk>
2019-10-03 13:43:12 +0200

    Initial commit

---
+++ src/main/java/com/kinoempire/demo/KinoEmpireApplication.java
@@ -0,0 +1,13 @@
+package com.kinoempire.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoEmpireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoEmpireApplication.class, args);
+    }
+
+}
