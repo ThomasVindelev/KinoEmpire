7437bfde26ca7301e515637939801cc1da519e7f
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2019-10-05 16:32:47 +0200

    getConnection();

---
+++ src/main/java/com/kinoempire/demo/Repository/Database.java
@@ -0,0 +1,20 @@
+package com.kinoempire.demo.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class Database {
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://den1.mysql5.gear.host/KinoEmpire?autoReconnect=true",
+                    "kinoempire",
+                    "Vc0Hz~9E!4bK");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
--- src/main/java/com/kinoempire/demo/Repository/MovieRepository.java
+++ src/main/java/com/kinoempire/demo/Repository/MovieRepository.java
@@ -3,22 +3,20 @@ package com.kinoempire.demo.Repository;
 import com.kinoempire.demo.Model.Movie;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Repository
-public class MovieRepository {
+public class MovieRepository extends Database {
 
-    private Connection connection;
     private PreparedStatement preparedStatement;
     private String query;
 
     public void addMovie(Movie movie) {
         query = "INSERT INTO movie (title, length, age_limit, fk_genre) VALUES (?,?,?,?)";
         try {
-            preparedStatement = connection.prepareStatement(query);
+            preparedStatement = getConnection().prepareStatement(query);
             preparedStatement.setString(1, movie.getTitle());
             preparedStatement.setInt(2, movie.getLength());
             preparedStatement.setInt(3, movie.getAge_limit());
@@ -32,7 +30,7 @@ public class MovieRepository {
     public void deleteMovie(int id) {
         query = "DELETE FROM movie WHERE id = ?";
         try {
-            preparedStatement = connection.prepareStatement(query);
+            preparedStatement = getConnection().prepareStatement(query);
             preparedStatement.setInt(1, id);
             preparedStatement.execute();
         } catch (SQLException e) {
@@ -45,7 +43,7 @@ public class MovieRepository {
                 "INNER JOIN genre ON fk_genre = genre.id";
 
         try {
-            preparedStatement = connection.prepareStatement(query);
+            preparedStatement = getConnection().prepareStatement(query);
             return preparedStatement.executeQuery();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -57,7 +55,7 @@ public class MovieRepository {
         query = "SELECT title, length, age_limit, genre" +
                 "INNER JOIN genre ON fk_genre = genre.id WHERE movie.id = ?";
         try {
-            preparedStatement = connection.prepareStatement(query);
+            preparedStatement = getConnection().prepareStatement(query);
             preparedStatement.setInt(1, id);
             return preparedStatement.executeQuery();
         } catch (SQLException e) {
