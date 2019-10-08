f67887d98317a994733aa1834a5298c3bf3c2fe4
JoachimGoodman <joac1087@stud.kea.dk>
2019-10-08 13:14:11 +0200

    MOJO objects

--- src/main/java/com/kinoempire/demo/Model/Theater.java
+++ src/main/java/com/kinoempire/demo/Model/Theater.java
@@ -2,7 +2,22 @@ package com.kinoempire.demo.Model;
 
 public class Theater {
 
-    int[][] seats;
+    private int seats;
+    private int rows;
 
+    public int getSeats() {
+        return seats;
+    }
 
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
 }
