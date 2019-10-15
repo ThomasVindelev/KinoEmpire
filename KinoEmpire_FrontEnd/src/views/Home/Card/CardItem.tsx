8243fc29c1daeec97a360ee0fbfdc6d4fc7956be
Nick Hansen <nick4630@stud.kea.dk>
2019-10-15 14:44:46 +0200

    frontend stats

--- KinoEmpire_FrontEnd/src/views/Home/Card/CardItem.tsx
+++ KinoEmpire_FrontEnd/src/views/Home/Card/CardItem.tsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card'
 import './cardItem.css';
 
-interface Props { 
+interface Props {
     cardTitle: string;
-    url: string;
-    favicon: string;
+    url?: string;
+    favicon?: string;
+    stat?: number;
 }
 interface State { }
 
@@ -22,16 +23,16 @@ class CardItem extends Component<Props, State> {
         return (
             <div className="cardItem">
                 <a href={this.props.url}>
-            <Card style={{ width: '18rem', background: "#343a40", marginRight: "15px" }}>
-                <Card.Body style={{textAlign: 'center'}}>
-                    <Card.Title >{this.props.cardTitle}</Card.Title>
-                    <Card.Text>
-                    <h1><i className={this.props.favicon}></i></h1>
-                        
-    </Card.Text>
-                </Card.Body>
-            </Card>
-            </a>
+                    <Card style={{ width: '18rem', background: "#343a40", marginRight: "15px" }}>
+                        <Card.Body style={{ textAlign: 'center' }}>
+                            <Card.Title >{this.props.cardTitle}</Card.Title>
+                            <Card.Text>
+                                <h1><i className={this.props.favicon}></i></h1>
+                                {this.props.stat ? <h2>{this.props.stat}</h2> : null}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </a>
             </div>
         )
     }
--- KinoEmpire_FrontEnd/src/views/Stats/Stats.tsx
+++ KinoEmpire_FrontEnd/src/views/Stats/Stats.tsx
@@ -1,21 +1,57 @@
 import React from 'react';
-import { MDBNotification } from 'mdbreact';
+import '../Home/Home.css';
+import CardItem from '../Home/Card/CardItem';
+import Spinner from 'react-bootstrap/Spinner';
 
 interface StatsState {
-  
+    totalSeats: number;
+    totalViewings: number;
+    loaded: boolean;
 }
 
 interface StatsProps {
-
+    
 }
 
 class Stats extends React.Component<StatsProps, StatsState> {
 
+    constructor(props: StatsProps) {
+        super(props);
+        this.state = {
+            totalSeats: 0,
+            totalViewings: 0,
+            loaded: false
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:5000/getSoldSeats')
+        .then(res => res.json())
+        .then(res => this.setState({ totalSeats: res }))
+        .then(() => {
+            fetch('http://localhost:5000/getViewingCount')
+            .then(res => res.json())
+            .then(res => this.setState({ totalViewings: res, loaded: true }))
+        });
+    }
+
     render() {
         return (
-            <div>
-                <p>Stats</p>
-            </div>
+            <React.Fragment>
+                {this.state.loaded ? <div className="Cards">
+                <CardItem url="#" cardTitle="Antal solgte sæder" favicon="fas fa-couch" stat={this.state.totalSeats} />
+                <CardItem url="#" cardTitle="Forestillinger i alt" favicon="fas fa-ticket-alt" stat={this.state.totalViewings} />
+                <CardItem url="#" cardTitle="Omsætning i kr" favicon="fas fa-dollar-sign" stat={this.state.totalSeats * 115} />
+            </div> : <Spinner
+            animation="grow"
+            style={{
+              display: "block",
+              position: "fixed",
+              top: "50%",
+              right: "50%"
+            }}
+          />}
+            </React.Fragment>
         )
     }
 
